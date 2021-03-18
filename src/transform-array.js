21b2acae84cbc40aeb1ff32969c4c8e832295373
Алексей <tov.kessler@yandex.ru>
2021-03-18 15:43:06 +0800

    feat: implement transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("error");
+  }
+
+  let result = [];
+
+
+  for(let i = 0; i < arr.length; i++) {
+
+    switch(arr[i]) {
+      case "--discard-next":
+        if (i < arr.length ) {
+          i++
+          
+          if (arr[i + 1] == "--discard-prev" || arr[i + 1] == "--double-prev" ) {
+            i++;
+          }
+        }
+        break;
+      case "--discard-prev":
+        i !== 0 ? result.pop() : null;
+        break;
+      case "--double-next":
+        i !== arr.length - 1 ?  result.push(arr[i + 1]) : null;
+        break;
+      case "--double-prev":
+        i !== 0 ? result.push(arr[i - 1]) : null;
+        break;
+      default: 
+        result.push(arr[i]);
+    }
+    
+  
+  }
+
+  
+  return result.filter(item => item != "--discard");
 };
